ce6ee83d60ccd65dee185e97f01e13bf96f29e09
emilykamiti <emilykamiti@gmail.com>
2024-01-25 07:59:40 +0300

    variable, dynamic typing, data types

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 let js = "amazing";
-if (js == "amazing") alert("JavaScript is FUN!");
 
-40 + 49 - 10;
 console.log(40 + 49 - 10);
+console.log("jonas");
+
+let firstName = "Emily"
+console.log(firstName);
+
+true;
+console.log(true)
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Emily");
+console.log(javascriptIsFun);
+
+
+//?you can change the  value  ot the varible
+//dynamic typing
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+
+let age;
+console.log(age)
+console.log(typeofage)
+
+age = 30;
+console.log(typeof age);
