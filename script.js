1d2d9f85c96436f1903f9962f0b612ff5eebd384
emilykamiti <emilykamiti@gmail.com>
2024-04-09 14:35:10 +0300

    java script operators

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-let js = "amazing";
+// let js = "amazing";
 
-console.log(40 + 49 - 10);
-console.log("jonas");
+// console.log(40 + 49 - 10);
+// console.log("jonas");
 
-let firstName = "Emily"
-console.log(firstName);
+// let firstName = "Emily"
+// console.log(firstName);
 
-true;
-console.log(true)
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// true;
+// console.log(true)
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof 23);
-console.log(typeof "Emily");
-console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof "Emily");
+// console.log(javascriptIsFun);
 
 
-//?you can change the  value  ot the varible
-//dynamic typing
-javascriptIsFun = "yes";
-console.log(typeof javascriptIsFun);
+// //?you can change the  value  ot the varible
+// //dynamic typing
+// javascriptIsFun = "yes";
+// console.log(typeof javascriptIsFun);
+// let PI = 3.1415;
 
-let age;
-console.log(age)
-console.log(typeofage)
+// let age;
+// console.log(age)
+// console.log(typeofage)
 
-age = 30;
-console.log(typeof age);
+// age = 30;
+// console.log(typeof age);
+
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+// var job = 'programmre';
+// job = 'teacher';
+
+//!mathematical operators
+const now = 2022;
+const ageEmily = now - 2001;
+const ageIsabella = now - 1999
+console.log(ageEmily, ageIsabella);
+console.log(ageEmily * 2, ageEmily / 10, 2 ** 3)
+
+const firstName = 'Emily';
+const lastName = 'Kamiti';
+
+console.log(firstName + ' ' + lastName);
+
+
+// !assignment operators
+let x = 10 + 5;
+x += 10; // x = x+10
+x *= 4;
+x++; // x = x +1
+x--;
+console.log(x);
+
+//! comparison operators
+
+console.log(ageEmily > ageIsabella);
+
+console.log(ageIsabella >= 18);
+
+const isFullage = ageIsabella >= 18;
